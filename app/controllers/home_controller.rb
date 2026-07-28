d85aa12a37bd97914fd2d3883420352c01bacdd8
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-07-28 14:22:21 +0200

    show actual hackatime projects

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
+require "net/http"
+require "json"
+
 class HomeController < ApplicationController
     before_action :set_logged_in
     def index
       if not @loggedin
         render "guest"
       end
+      get_hackatime_projects
     end
 
     private
@@ -15,5 +19,21 @@ class HomeController < ApplicationController
             @voyage = Voyage.find(@user.voyage)
           end
         end
+      end
+      def get_hackatime_projects
+        token = @user.token
+
+        date = "2026-08-02T22:00:00.000Z"
+        # TODO: REPLACE DATE BEFORE LAUNCH
+        date = "2026-07-07T22:00:00.000Z"
+
+        url = URI("https://hackatime.hackclub.com/api/v1/authenticated/projects?include_archived=false&projects=&since=&until=&until_date=&start="+date+"&end=&start_date=&end_date=")
+        req = Net::HTTP::Get.new(url)
+        req["Authorization"] = "Bearer " + token
+        res = Net::HTTP.start(url.hostname, url.port, use_ssl: url.scheme == "https") { |http|
+          http.request(req)
+        }
+        data = JSON.parse(res.body)
+        @projects = data["projects"]
       end
 end
