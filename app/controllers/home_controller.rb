6647e528e1c100f6d739de6ec85b2286d18f3a50
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-08-04 15:37:41 +0200

    Always fetch all hackatime projects!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,8 @@ class HomeController < ApplicationController
       if not @loggedin
         render "guest"
       else
-        if @voyage == nil
-          get_hackatime_projects
-        else
-          @projects = []
-          update_voyage_time
-        end
+        get_hackatime_projects
+        update_voyage_time
         generate_hackatime_text
         get_next_island
         generate_desc_trimmed
@@ -20,18 +16,13 @@ class HomeController < ApplicationController
         if @voyage == nil || @voyage.hackatime.strip.empty?
           return
         end
-        token = @user.token
-        params = { projects: @voyage.hackatime }
-        encoded_query = URI.encode_www_form(params)
-        url = URI("https://hackatime.hackclub.com/api/v1/authenticated/projects?include_archived=false&#{encoded_query}&since=&until=&until_date=&start="+ysws_start+"&end=&start_date=&end_date=")
-        req = Net::HTTP::Get.new(url)
-        req["Authorization"] = "Bearer " + token
-        res = Net::HTTP.start(url.hostname, url.port, use_ssl: url.scheme == "https") { |http|
-          http.request(req)
-        }
-        data = JSON.parse(res.body)
-        p = data["projects"][0]["total_seconds"]
-        @voyage.total_seconds = p
-        @voyage.save
+        
+        for a in @projects
+          if a["name"] == @voyage.hackatime
+            @voyage.total_seconds = a["total_seconds"]
+            @voyage.save
+            return
+          end
+        end
       end
 end
