e948f09020ac298cef79397877bb713e5230af59
ingobeans <127751491+ingobeans@users.noreply.github.com>
2026-08-08 03:37:44 +0200

    Send reviewer notifications to specific reviewer channel, not the admin DM

--- app/controllers/voyage_controller.rb
+++ app/controllers/voyage_controller.rb
@@ -56,7 +56,7 @@ class VoyageController < ApplicationController
       return
     end
     # valid ship probably !
-    aid = slack_open_conversation(ENV["ADMIN_SLACK_ID"])
+    aid = ENV["REVIEWER_CHANNEL_ID"]
     id = slack_open_conversation(@user.uid)
 
     @voyage.ship_status = 1
@@ -66,7 +66,7 @@ class VoyageController < ApplicationController
     if @voyage_name_trim == nil or @voyage_name_trim.blank?
       generate_desc_trimmed()
     end
-    slack_send_message_conversation(aid, ":shipitparrot: New ship alert !!\n<@" + @user.uid + "> just shipped `"+ @voyage_name_trim + "`, " + (@voyage.total_seconds / 60 / 60).to_i.to_s + " hours.\nPrices: `" + @voyage.cargo + "`")
+    slack_send_message_conversation(aid, ":shipitparrot: New ship alert !!\n<@" + @user.uid + "> just shipped `"+ @voyage_name_trim + "`, " + (@voyage.total_seconds / 60 / 60).to_i.to_s + " hours.\nPrices: `" + @voyage.cargo + "`\n<#{reviewer_url+"/edit/"+@voyage.id.to_s}|Review>")
 
     # send message to user!
     slack_send_message_conversation(id, "Good work captain !! Your project has been shipped! :yay::yay:\n\nYour project will be reviewed soon and after that you will receive your prices! :sos-heidi-treasure::3c:\nDM <@" + ENV["ADMIN_SLACK_ID"] + "> if you have any questions!\n\n/pirate orph' <3")
